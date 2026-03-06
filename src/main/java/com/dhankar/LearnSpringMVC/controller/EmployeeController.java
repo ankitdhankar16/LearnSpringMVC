965731d036f27e06f4e7a991a54935479519dd25
Ankit Dhankar <adhankar1604@gmail.com>
2026-03-06 21:34:17 +0530

    learned the use of PathVariable and RequestParam

--- src/main/java/com/dhankar/LearnSpringMVC/controller/EmployeeController.java
+++ src/main/java/com/dhankar/LearnSpringMVC/controller/EmployeeController.java
@@ -1,17 +1,16 @@
 package com.dhankar.LearnSpringMVC.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping(path="/employees")
 public class EmployeeController {
-    @GetMapping
-    public String getAllEmployees(){
-        return "All Employees";
+
+    @GetMapping()
+    public String getAllEmployees(@RequestParam(required = false) String sortBy , @RequestParam (required = false)String filter){
+        return "All Employees" + sortBy + filter;
     }
+
     @GetMapping("/{id}")
     public String getEmployeeById(@PathVariable Long id){
         return "Employee id is "+ id;
